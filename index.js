d55e9f0f3ce31872025afb7f4937f05dd7337b68
Gabriel1470 <gabriel1470bg@gmail.com>
2022-07-15 18:27:02 +0200

    esercizio 3?

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ console.log(Sum(5,6))
 
 const crypto = require("crypto");
 
-const id = crypto.randomBytes(10).toString("hex");
+ module.exports= id = crypto.randomBytes(10).toString("hex");
 
-console.log(id)
+/*  console.log(id) */
---
+++ recall.js
@@ -0,0 +1,4 @@
+const random = require("./index") 
+
+
+console.log(random)
