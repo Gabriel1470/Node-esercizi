a747c8fbc24430476deb97ab111f4bffd3ca3d9c
Gabriel1470 <gabriel1470bg@gmail.com>
2022-07-20 17:07:13 +0200

    Esercizio 04

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
+/* esercizio 01 */
 function Sum(a, b){
   return a+b
 }
 
 console.log(Sum(5,6))
 
-
+/* esercizio 02  */
 const crypto = require("crypto");
 
  module.exports= id = crypto.randomBytes(10).toString("hex");
 
 /*  console.log(id) */
+
+/* esercizio 04 */
+
+module.exports= function Saluta(msg){
+  return console.log(`${msg}`)
+}
+
+function Ciao(msg){
+  console.log(`${msg}`)
+}
+
+export default {Ciao}
--- recall.js
+++ recall.js
@@ -1,4 +1,20 @@
-const random = require("./index") 
+/* esercizio 04 */
+const Saluta = require("./index.js")
+Saluta('Hollo Jagmagar')
 
 
+
+import {Ciao} from "./index.js"
+
+Ciao('Engardium Leviousa')
+
+
+
+
+
+
+
+/* esercizio 03 */
+const random = require("./index.js") 
 console.log(random)
+
